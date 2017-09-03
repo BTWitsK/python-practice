f760accff1b8be0b6436243ad07414451ab88721
BTWitsK <BTWitsK@Gmail.com>
2017-09-03 02:59:51 -0500

    Started working on solution 5

--- Python/problemSet1/Solution5.py
+++ Python/problemSet1/Solution5.py
@@ -3,4 +3,12 @@
 #Enter fixed costs, price per unit and cost per unit into your program.
 #Compute the break even point by dividing fixed costs by the difference of price per unit and cost per unit.
 
+#Asks the user for the fixed cost of the item, and converts it to a float
+fixedCost = float(input('Enter fixed cost of the item: '))
+
+#Asks the user for the price per unit of the item and converts it to a float
+pricePerUnit = float(input('Enter the price per unit of the item'))
+
+#Asks the user for the cost per unit of the item and converts it to a float
+costPerUnit = float(input('Enter the cost for each item'))
 
