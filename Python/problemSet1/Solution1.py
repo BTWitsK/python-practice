06befec2695aa532b4dfc5550f82fbd38d999ea3
BTWitsK <BTWitsK@Gmail.com>
2017-09-30 00:59:16 -0500

    updated the first solution

--- Python/problemSet1/Solution1.py
+++ Python/problemSet1/Solution1.py
@@ -7,15 +7,15 @@ secondNum = float(input('Enter the second number: '))
 #Remember input() takes whatever is typed as a string, float() changes this to a float to allow for decimals
 
 #Compute the sum then change type to string to display
-sum = str(firstNum + secondNum)
-print('The sum of your two numbers is: ' + sum)
+add = str(firstNum + secondNum)
+print('The sum of your two numbers is: {}'.format(add))
 
 #Does the same as the above code segment but for the product
 product = str(firstNum * secondNum)
-print('The product of your two numbers is: ' + product)
+print('The product of your two numbers is: {}'.format(product))
 
 difference = str(firstNum - secondNum)
-print('The difference between your two numbers is: ' + difference)
+print('The difference between your two numbers is: {}'.format(difference))
 
-quotient = str(firstNum // secondNum)
-print('The quotient of the two numbers is: ' + quotient)
+quotient = str(firstNum / secondNum)
+print('The quotient of the two numbers is: {}'.format(quotient))
